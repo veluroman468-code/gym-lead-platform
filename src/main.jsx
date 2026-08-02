087fcc400763fb7d84a6be79a0a614d38ed52a82
veluroman468-code <veluroman468@gmail.com>
2026-08-02 23:42:09 +0530

    Add main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GymLeadTool from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GymLeadTool />
+  </React.StrictMode>
+);
